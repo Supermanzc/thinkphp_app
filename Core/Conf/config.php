d50a74937f6639c35b34c41e9fc3de73ac1b7173
zhongchuan <512749461@qq.com>
2014-04-02 09:50:42 +0800

    zc

---
+++ Core/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'SHOW_PAGE_TRACE'=>true, //开启trace
+	'APP_GROUP_LIST'=>'Home, Admin', //分组
+	'DEFAULT_GROUP'=>'Home', //默认分组
+
+);
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//加载应用名称
+define('APP_NAME', 'Core');
+//加载应用目录
+define('APP_PATH', './Core/');
+//开启debug
+define('APP_DEBUG', true);
+
+//加载应用入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+?>
