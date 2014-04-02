f80ef6d5abb4061641a961988fd98312dcb7f2b0
zhongchuan <512749461@qq.com>
2014-04-02 11:33:07 +0800

    zc

--- Core/Conf/config.php
+++ Core/Conf/config.php
@@ -2,8 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'SHOW_PAGE_TRACE'=>true, //开启trace
-	'APP_GROUP_LIST'=>'Home, Admin', //分组
+	'APP_GROUP_LIST'=>'Home,Admin', //分组
 	'DEFAULT_GROUP'=>'Home', //默认分组
-
+	
+	'LOAD_EXT_CONFIG'=>'db',
+	
 );
 ?>
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ thinkphp_app
 1.验证码
 2.图片上传
 3.图片批量上传
+
+
+问题1.无法加载模块问题
+config.php  => APP_GROUP_LIST中每个分组用,分割中间不能有空格
